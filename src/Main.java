05db2a0486c862f4e033e34f3bb10d8d0a71e523
yuriy-kolosov <yuriy.v.kolosov@gmail.com>
2024-10-06 00:04:07 +0300

    Тестовое техническое собеседование 04-10-2024 - Начальный коммит

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Тестовое техническое собеседование 04-10-2024 - Задача");
+        System.out.println();
+
+    }
+}
